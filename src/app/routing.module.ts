a85db720d831b86a3fc61b211bc4c612631ae607
marlonlom <malm.gm@gmail.com>
2018-01-03 23:58:02 -0500

    20180103: adjusted some minor responsive css styles

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import {LoginComponent} from './pages/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }, {
-    path: 'login',
-    component: LoginComponent
   }, {
     path: '**',
     component: NotFoundComponent
