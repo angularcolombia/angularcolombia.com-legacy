e00910d7a21239d798c7fe4e61f0707aa68d3581
Marlon José López Meza <malm.gm@gmail.com>
2018-01-26 20:55:10 -0500

    20180126: removed console.log usages

--- dev-server/ngco-meetups/google_staticmaps.js
+++ dev-server/ngco-meetups/google_staticmaps.js
@@ -17,7 +17,6 @@ router.get('/staticmap', (req, resp) => {
             .concat('/staticmap?center=#MAP_LOCATION&zoom=16&scale=1&size=260x200&maptype=roadmap&format=jpg&sensor=false&visual_refresh=true&markers=size:normal|color:0xff0000|label:A|#MAP_LOCATION')
             .replace('#MAP_LOCATION', map_position).replace('#MAP_LOCATION', map_position);
 
-    console.log('image_url=',encodeURI(image_url));
     request(encodeURI(image_url)).pipe(resp);
 });
 
